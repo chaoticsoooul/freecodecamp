81b9a497aa55dd75bfc98a9a367ec1e8b586d229
Natalie Leung <86335550+chaoticsoooul@users.noreply.github.com>
2024-08-31 16:42:14 +0800

    Create script.js

---
+++ javascript-algorithms-and-data-structures/script.js
@@ -0,0 +1,28 @@
+const input = document.getElementById('text-input');
+const button = document.getElementById('check-btn');
+const result = document.getElementById('result');
+
+function isPalidrome(str){
+return str == str.split('').reverse().join('')
+}
+
+const clickButton = () => {
+  result.innerText = "";
+  
+  const text = input.value;
+  const modifiedText = input.value.replace(/[^0-9a-z]/gi, '').toLowerCase();
+
+
+  if(!text){
+    alert("Please input a value");
+  }
+
+  if(isPalidrome(modifiedText)){
+    result.innerText = `${text} is a palindrome`;    
+  } else {
+   result.innerText = `${text} is not a palindrome`;
+  }
+}
+
+
+button.addEventListener("click", clickButton);
