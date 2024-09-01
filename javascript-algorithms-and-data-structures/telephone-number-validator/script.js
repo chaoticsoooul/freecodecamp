85d78f3d17b11f6c8eb7699bab5d8b96fb309c18
Natalie Leung <natal6ie@gmail.com>
2024-09-01 23:34:33 +0900

    Modify UI

--- javascript-algorithms-and-data-structures/telephone-number-validator/script.js
+++ javascript-algorithms-and-data-structures/telephone-number-validator/script.js
@@ -7,22 +7,24 @@ const regex1 = /^[1]?\s?\d{3}(-|\s)?\d{3}(-|\s)?\d{4}$/;
 const regex2 = /^[1]?\s?(\()\d{3}(\))(-|\s)?\d{3}(-|\s)?\d{4}$/;
 
 const check = () => {
+    result.innerText = "";
+    
   if(input.value === ""){
     alert("Please provide a phone number");
   } else {
+    result.style.visibility = "visible";
     if(regex1.test(input.value) || regex2.test(input.value)){
       result.innerText = `Valid US number: ${input.value}`;} else {
       result.innerText = `Invalid US number: ${input.value}`
     }
-    
   }
 };
 
 const clear = () => {
   input.value = "";
   result.innerText = "";
+  result.style.visibility = "hidden";
 };
 
-
-checkBtn.addEventListener("click", check)
-clearBtn.addEventListener("click", clear)
+checkBtn.addEventListener("click", check);
+clearBtn.addEventListener("click", clear);
