f80aea31869c8d3d3fa014d7acc1ceee91584033
Natalie Leung <natalieleung@NataliedeMacBook-Air.local>
2024-09-01 22:43:06 +0900

    Add new project

---
+++ javascript-algorithms-and-data-structures/telephone-number-validator/script.js
@@ -0,0 +1,28 @@
+const input = document.getElementById("user-input");
+const checkBtn = document.getElementById("check-btn");
+const clearBtn = document.getElementById("clear-btn");
+const result = document.getElementById("results-div");
+
+const regex1 = /^[1]?\s?\d{3}(-|\s)?\d{3}(-|\s)?\d{4}$/;
+const regex2 = /^[1]?\s?(\()\d{3}(\))(-|\s)?\d{3}(-|\s)?\d{4}$/;
+
+const check = () => {
+  if(input.value === ""){
+    alert("Please provide a phone number");
+  } else {
+    if(regex1.test(input.value) || regex2.test(input.value)){
+      result.innerText = `Valid US number: ${input.value}`;} else {
+      result.innerText = `Invalid US number: ${input.value}`
+    }
+    
+  }
+};
+
+const clear = () => {
+  input.value = "";
+  result.innerText = "";
+};
+
+
+checkBtn.addEventListener("click", check)
+clearBtn.addEventListener("click", clear)
